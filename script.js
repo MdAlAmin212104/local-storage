6ac3d071be7d978b43be25b1134d31ddc9fd26f2
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-03-05 14:07:36 +0600

    handleClick added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("this is a test");
+const handleClick = ()=>{
+      const productName = document.getElementById('Product-name');
+      const ProductQuantity = document.getElementById('Product-quantity');
+      const name = productName.value;
+      const quantity = ProductQuantity.value;
+      productName.value = '';
+      ProductQuantity.value = '';
+      console.log(name, quantity);
+}
