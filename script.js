5f056e4dbd631dfb0e04e6e1f4ebb604969e785d
MdAlAmin212104 <mdalamin212104@gmail.com>
2024-03-05 14:34:46 +0600

    localStorage Product get result

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const listItem = document.getElementById('list-item');
+
 const handleClick = ()=>{
       const productName = document.getElementById('Product-name');
       const ProductQuantity = document.getElementById('Product-quantity');
@@ -5,5 +7,24 @@ const handleClick = ()=>{
       const quantity = ProductQuantity.value;
       productName.value = '';
       ProductQuantity.value = '';
-      console.log(name, quantity);
+      elementDisplay(name, quantity);
+}
+
+const elementDisplay = (name, quantity)=>{
+      const li = document.createElement('li');
+      li.innerText = `${name} ${quantity}`;
+      listItem.appendChild(li);
+}
+const getStoragesShoppingCart = ()=>{
+      let cart = {};
+      const storedCart = localStorage.getItem('cart');
+      if(storedCart){
+            cart = JSON.parse(storedCart);
+      }
+      return cart;
+}
+
+const saveProductToLocalStorage = (product, quantity)=>{
+
+
 }
